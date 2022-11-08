4604a3165e950f39d96f571bc2f3b478c38a3730
David Rambo <davidrambo@mailfence.com>
2022-11-07 16:05:47 -0800

    Delete TODO message

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -110,9 +110,6 @@ public class Model extends Observable {
         boolean changed;
         changed = false;
 
-        // TODO: Modify this.board (and perhaps this.score) to account
-        // for the tilt to the Side SIDE. If the board changed, set the
-        // changed local variable to true.
         if (side != Side.NORTH) {
             board.setViewingPerspective(side);
         }
