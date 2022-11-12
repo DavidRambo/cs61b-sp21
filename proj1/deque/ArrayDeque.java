e84d38c74eb6bad79895077b68075d0b3e6ac74a
David Rambo <davidrambo@mailfence.com>
2022-11-12 13:49:27 -0800

    Proj1 checkpoint

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -23,10 +23,12 @@ public class ArrayDeque<T> {
         size = 0;
     }
 
+    /** Returns true if deque is empty. */
     public boolean isEmpty() {
         return size == 0;
     }
 
+    /** Returns the size of the deque as an int. */
     public int size() {
         return size;
     }
@@ -39,9 +41,11 @@ public class ArrayDeque<T> {
         }
 
         // Check for circular repositioning of front.
+        // Note that there is no risk of front meeting back due to above resize check.
         if (front == 0) {
             front = items.length - 1;
-        } else {
+        } else if (!isEmpty()) {
+            // Non-empty list, so move front.
             front -= 1;
         }
         items[front] = item;
@@ -50,46 +54,142 @@ public class ArrayDeque<T> {
 
     /** Adds an item to the end of the deque. */
     public void addLast(T item) {
-        // TODO
+        // Check whether array is full.
+        if (size == items.length) {
+            resize(2 * size);
+        }
+
+        // Check for circular repositioning of back.
+        if (back == items.length - 1) {
+            back = 0;
+        } else if (!isEmpty()) {
+            // Non-empty list, so move back.
+            back += 1;
+        }
+
+        items[back] = item;
+        size += 1;
     }
 
     /** Removes the item at the front of the deque and returns it.
      * If no item exists, then returns null.
      */
     public T removeFirst() {
-        // TODO
-        return null;
+        T removed = items[front];
+
+        if (removed == null) {
+            return null;
+        } else if (front == items.length - 1 && front != back) {
+            // Circular condition and not a one-item array.
+            front = 0;
+        } else if (front != back) {
+            front += 1;
+        }
+
+        items[front] = null;
+        size -= 1;
+
+        // Check usage ratio and resize if < 0.25
+        // but do not shrink smaller than length of 8.
+        if ((items.length > 8) && ((float) size/ items.length) < 0.25) {
+            resize(items.length / 2);
+        }
+        return removed;
     }
 
     /** Removes the item at the end of the deque and returns it.
-     * If no item exists, then returns null.
-     */
+     * If no item exists, then returns null. */
     public T removeLast() {
-        // TODO
-        return null;
+        T removed = items[back];
+
+        if (removed == null) {
+            return null;
+        } else if (back == 0 && front != back) {
+            // Circular condition and more than one item in array.
+            back = items.length - 1;
+        } else if (front != back) {
+            // Check for 1-item array; i.e. front == back
+            back -= 1;
+        }
+
+        items[back] = null;
+
+        size -= 1;
+
+        // Check usage ratio and resize if < 0.25
+        // but do not shrink smaller than length of 8.
+        if ((items.length > 8) && ((float) size/ items.length) < 0.25) {
+            resize(items.length / 2);
+        }
+        return removed;
+    }
+
+    /** Gets the item at the given index, where 0 is the front, 1 the next item, etc.
+     * If no such item exists, returns null.
+     * In a non-circular array, location = front + index.
+     * In a circular array, location can still be front + index, if that is < length.
+     * If that is > length, then subtract length from it.
+     * Example:
+     * [2, 3, 4, 5, 6, 7, null, null, null, null, 0, 1]
+     * front = 10, index = 6
+     * location = 4 -> items[4] = 6
+     * length = 12
+     * 6 + 10 = 16 -> 16 - 12 = 4
+     * */
+    public T get(int index) {
+        int location = front + index;
+        // Check for circularity
+        if (location > items.length) {
+            location = location - items.length;
+        }
+        return items[location];
+    }
+
+    /** Prints the items in the deque from front to back, separated by a space. */
+    public void printDeque() {
+        int position = front;
+
+        // Iterate from front to back, checking for end of array (circular).
+        // If circular, then position will always be > back.
+        while (position <= back || position < items.length) {
+            System.out.print(items[position] + " ");
+            position += 1;
+        }
+
+        // If end of array is met and back is not, then iterate from items[0] to back.
+        if (back < position) {
+            position = 0;
+            while (position <= back) {
+                System.out.print(items[position] + " ");
+                position += 1;
+            }
+        }
+
+//        System.out.print(items[position + 1]);
+
+        System.out.println();
     }
 
     /** Resizes the array.
-     * Note that because the capacity changes by factors of 2,
-     * capacity will always be divisible by 4. This integer is
-     * used to reset the front position. */
+     * Note that because the capacity changes by factors of 2, and starting capacity is 8,
+     * capacity will always be divisible by 4. This integer is used to reset the front position. */
     public void resize(int capacity) {
         T[] temp = (T[]) new Object[capacity];
         int start = capacity / 4;
         // For circular array, front will be at higher index than back.
         if (front > back) {
             // First, copy from front of deque to end of array.
-            int end = items.length - 1;
-            System.arraycopy(items, front, temp, start, end);
+            int numberOfItems = items.length - front;
+            System.arraycopy(items, front, temp, start, numberOfItems);
             // Calculate where to pick up for the copy operation.
-            int secondStart = start + end;
+            int secondStart = start + numberOfItems;
             // Then, copy from beginning of array to back of deque.
-            System.arraycopy(items, 0, temp, secondStart, back);
+            System.arraycopy(items, 0, temp, secondStart, (size - numberOfItems));
         } else {
             System.arraycopy(items, front, temp, start, size);
         }
         front = start;
-        back = front + size;
+        back = front + size - 1;
         items = temp;
     }
 }
--- proj1/deque/ArrayDequeTest.java
+++ proj1/deque/ArrayDequeTest.java
@@ -26,21 +26,117 @@ public class ArrayDequeTest {
     @Test
     /* Add to deque to resize it. Check size.*/
     public void addResizeTest() {
-        ArrayDeque<Integer> ad1 = new ArrayDeque<>(5);
+        ArrayDeque<Integer> ad1 = new ArrayDeque<>();
         ad1.addFirst(4);
-//        ad1.addLast(6);
         ad1.addFirst(3);
         ad1.addFirst(2);
         ad1.addFirst(1);
         ad1.addFirst(0);
-//        ad1.addLast(7);
-//        ad1.addLast(8);
-//        ad1.addLast(9);
         ad1.addFirst(3);
         ad1.addFirst(2);
         ad1.addFirst(1);
         ad1.addFirst(0);
         ad1.addFirst(0);
         assertEquals(10, ad1.size());
+    }
+
+    @Test
+    /* Add to front and back of deque to resize it. Check size. */
+    public void frontBackResizeTest() {
+        ArrayDeque<Integer> ad1 = new ArrayDeque<>(5);
+        ad1.addFirst(4);
+        ad1.addLast(6);
+        ad1.addFirst(3);
+        ad1.addFirst(2);
+        ad1.addFirst(1);
+        ad1.addFirst(0);
+        ad1.addLast(7);
+        ad1.addLast(8);
+        ad1.addLast(9);
+        ad1.addFirst(3);
+        ad1.addFirst(2);
+        ad1.addFirst(1);
+        ad1.addFirst(0);
+        ad1.addFirst(0);
+        ad1.addLast(13);
+        ad1.addLast(22);
+        assertEquals(17, ad1.size());
+    }
+
+    @Test
+    /* Adds items to the back to cause circular repositioning and resize. */
+    public void addLastResizeTest() {
+        ArrayDeque<Integer> ad1 = new ArrayDeque<>(0);
+        ad1.addLast(1);
+        ad1.addLast(2);
+        ad1.addLast(3);
+        ad1.addLast(4);
+        ad1.addLast(5);
+        ad1.addLast(6);
+        ad1.addLast(7);
+        ad1.addLast(8);
+        ad1.addLast(9);
+        assertEquals(10, ad1.size());
+    }
+
+    @Test
+    /* Adds and removes items from empty, 1-item, and 2+ item deques.
+    * Removes items to cause resize. */
+    public void addRemoveTest() {
+        ArrayDeque<Integer> adi = new ArrayDeque<>();
+        assertNull(adi.removeFirst());
+        assertNull(adi.removeLast());
+
+        adi.addLast(0);
+        int removed = adi.removeFirst();
+        assertEquals(0, removed);
+        assertNull(adi.removeFirst());
+        assertNull(adi.removeLast());
+        adi.addLast(1);
+        removed = adi.removeLast();
+        assertEquals(1, removed);
+        assertTrue(adi.isEmpty());
+        adi.addFirst(1);
+        removed = adi.removeFirst();
+        assertTrue(adi.isEmpty());
+        assertEquals(1, removed);
+
+        for (int i = 0; i < 25; i++) {
+            if (i % 2 == 0) {
+                adi.addFirst(i);
+            } else {
+                adi.addLast(i);
+            }
+        }
+        assertEquals(25, adi.size());
+
+        for (int i = 0; i < 18; i++) {
+            adi.removeFirst();
+        }
+    }
+
+    @Test
+    /* Creates a deque with circularity and gets items. */
+    public void getPrintTest() {
+        ArrayDeque<Integer> adi = new ArrayDeque<>();
+        adi.addLast(6);
+        adi.addLast(7);
+        adi.addFirst(5);
+        adi.addFirst(4);
+        adi.addFirst(3);
+        adi.addFirst(2);
+        adi.addFirst(1);
+        adi.addFirst(0);
+        // Array should look like: [2, 3, 4, 5, 6, 7, 0, 1]
+        // Front = 6, Back = 5
+        int getFront = adi.get(0);
+        int getBack = adi.get(7);
+        int getSecond = adi.get(1);
+        assertEquals(0, getFront);
+        assertEquals(7, getBack);
+        assertEquals(1, getSecond);
+        String testPrint = "0 1 2 3 4 5 6 7\n";
+        System.out.println(testPrint);
+        adi.printDeque();
     }
 }
