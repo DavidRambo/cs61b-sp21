789692c6b68699f8a7424540936afe498f7461da
David Rambo <davidrambo@mailfence.com>
2023-01-04 12:08:50 -0800

    Start init command

--- proj2/gitlet/Repository.java
+++ proj2/gitlet/Repository.java
@@ -9,7 +9,7 @@ import static gitlet.Utils.*;
  *  TODO: It's a good idea to give a description here of what else this Class
  *  does at a high level.
  *
- *  @author TODO
+ *  @author David Rambo
  */
 public class Repository {
     /**
@@ -20,10 +20,28 @@ public class Repository {
      * variable is used. We've provided two examples for you.
      */
 
+    // Array of branch names
+    public String[] branches;
+
     /** The current working directory. */
     public static final File CWD = new File(System.getProperty("user.dir"));
     /** The .gitlet directory. */
     public static final File GITLET_DIR = join(CWD, ".gitlet");
 
     /* TODO: fill in the rest of this class. */
+
+    /** Init command to initialize the Gitlet version-control system in the current
+        directory. If such a system already exists, it will instead print an error message. */
+    public static void init() {
+        if (GITLET_DIR.exists()) {
+            System.out.println("A Gitlet version-control system already exists in the current directory.");
+            System.exit(0);
+        }
+        Commit initial = new Commit("initial commit", null);
+        // TODO: What is a UID?
+    }
+
+    /* TODO: Status command. */
+
+    /* TODO: Add command. */
 }
