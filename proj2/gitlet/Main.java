707c3ab247d7ad8faaae4954ed6caef4424a29a6
David Rambo <davidrambo@mailfence.com>
2023-01-26 11:08:39 -0800

    Add reset command to Main

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -92,6 +92,12 @@ public class Main {
                     exitMessage("Incorrect operands");
                 Repository.find(args[1]);
                 break;
+            case "reset":
+                if (args.length != 2) {
+                    exitMessage("Incorrect operands");
+                }
+                Repository.reset(args[1]);
+                break;
             default:
                 exitMessage("No command with that name exists.");
         }
